bbf87fa5fa38583c56588f733919ffc31cdf00b6
Yaga <jeyaganesh.ranjit@gmail.com>
2014-05-14 01:47:07 +0200

    login style

--- neighbapp/library/Class/Form/Bootstrap/LoginVertical.php
+++ neighbapp/library/Class/Form/Bootstrap/LoginVertical.php
@@ -14,7 +14,12 @@ class Class_Form_Bootstrap_LoginVertical extends Twitter_Bootstrap_Form_Vertical
             'placeholder'   => 'E-mail',
             'prepend'       => '@',
             'class'         => 'focused'
-            ))->removeDecorator('label');
+            ))->removeDecorator('label')->addValidator('regex', false, array(
+    '/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}/',
+    'messages'=>array(
+    'regexNotMatch'=>'The email is not valid'
+    )    
+));;
         
         $this->getElement('password')->removeDecorator('label');
         
